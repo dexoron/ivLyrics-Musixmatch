b438354b4c13a3d653da9dc990dc8add15c9a5ee
ivLis <me@ivl.is>
2026-03-17 23:23:49 +0900

    4.1.6

--- NoticeSystem.js
+++ NoticeSystem.js
@@ -97,7 +97,7 @@ const getCurrentVersion = () => {
         window.Utils?.getCurrentVersion?.() ||
         window.CONFIG?.version ||
         window.ivLyricsVersion ||
-        "4.1.5"
+        "4.1.6"
     );
 };
 
--- Utils.js
+++ Utils.js
@@ -736,7 +736,7 @@ const Utils = {
   /**
    * Current version of the ivLyrics app
    */
-  currentVersion: "4.1.5",
+  currentVersion: "4.1.6",
 
   /**
    * Check for updates from remote repository
