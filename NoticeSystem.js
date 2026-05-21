fd45cb2da07a79cad7678d6ee3f950f042a43cd6
ivLis <me@ivl.is>
2026-05-21 20:32:09 +0900

    update

--- NoticeSystem.js
+++ NoticeSystem.js
@@ -97,7 +97,7 @@ const getCurrentVersion = () => {
         window.Utils?.getCurrentVersion?.() ||
         window.CONFIG?.version ||
         window.ivLyricsVersion ||
-        "5.0.1"
+        "5.0.2"
     );
 };
 
