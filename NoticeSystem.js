5c7f82cec290912676a7e1c036c291418935c932
ivLis <me@ivl.is>
2026-05-24 16:33:46 +0900

    5.2.0

--- NoticeSystem.js
+++ NoticeSystem.js
@@ -97,7 +97,7 @@ const getCurrentVersion = () => {
         window.Utils?.getCurrentVersion?.() ||
         window.CONFIG?.version ||
         window.ivLyricsVersion ||
-        "5.1.9"
+        "5.2.0"
     );
 };
 
--- Utils.js
+++ Utils.js
@@ -1070,7 +1070,7 @@ const Utils = {
   /**
    * Current version of the ivLyrics app
    */
-  currentVersion: "5.1.9",
+  currentVersion: "5.2.0",
 
   /**
    * Check for updates from remote repository
